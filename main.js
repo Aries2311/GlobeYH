6774068989076fcf62c5ade63c5921ffb05b4901
Aries2311 <aries.aquino1109@gmail.com>
2025-09-27 12:25:17 +0800

    zoominpincity

--- main.js
+++ main.js
@@ -65,7 +65,6 @@ function renderGlobeMarkers(data) {
             // On Click Event: Mag-zoom in at magpakita ng Pinning Modal
             img.onclick = (event) => {
                 event.stopPropagation();
-
                 
                 // Ipakita ang Pinning Modal
                 const status = d.is_pinned ? 'Yes' : 'No';
@@ -195,8 +194,8 @@ function displaySearchResults(results) {
                 const li = document.createElement('li');
                 li.textContent = city.label;
                 li.onclick = () => {
-            
-                   
+                    // Mag-zoom in sa pinili na lokasyon
+                    world.pointOfView({ lat: city.lat, lng: city.lng, altitude: 0.5 }, 1000);
                     
                     searchResultsList.style.display = 'none';
                     searchInput.value = '';
